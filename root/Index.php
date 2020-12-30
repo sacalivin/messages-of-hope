40986f57a276def254ba5eaa705c3fb0aa5774c6
sacalivin <obirisacalivin123@gmail.com>
2020-12-30 06:09:47 +0300

    first commit

---
+++ root/Index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <link rel="stylesheet" href="../styles/w3.css" type="text/css">
+        <link rel="stylesheet" href="../styles/index.css" type="text/css">
+
+    </head>
+
+    <body>
+
+        <nav id="nav">
+            Hello people! <br /> Hope all is well with you.
+        </nav>
+        <section id="section">
+            <article>
+                <h1>Growing in Christ</h1>
+                <div class="message w3-card-4 w3-pale-green w3-padding " >
+                    <h3>Breath and Grow</h3>
+                    “In the matchless gift of His Son, God has encircled the
+                    whole world with an atmosphere of grace as real as the air
+                    which circulates around the globe. All who <em>choose to breathe</em>
+                    this life-giving atmosphere <em>will live and grow</em> up to
+                    the stature of men and women in Christ Jesus. “ (STC,68)
+
+                    Friend I ask you to choose to breath the grace of God. It 
+                    is free just as air around us.
+
+
+                </div>
+                <div class="message w3-card-4  w3-pale-yellow w3-padding w3-margin-top" >
+                    <h3>A right Turn wright to the point</h3>
+                    “As the ﬂower turns to the sun, that the bright beams
+                    may aid in perfecting its beauty and symmetry,” (STC,68)
+                    Friend I think it is something admirable to you to
+                    develop into the likeness of Christ, I want to assure you today that
+                    it is possible. All you need is to humble yourself like a
+                    flower and turn onto the Sun of Righteousness then the
+                    heavenly light will shine up you and you character will be
+                    transformed onto the likeness of Christ.
+
+                </div>
+                <div class="message w3-panel w3-container w3-margin-top w3-pale-blue">
+                <h3>Without Christ We can absolutely do nothing.</h3>
+                Jesus teaches the same thing when He says, 
+                “Abide in Me, and I in you. As the branch cannot bear 
+                fruit of itself, except
+                it abide in the vine; no more can ye, except ye abide in Me.... 
+                <em>Without Me ye can do nothing.”</em> John15:4,5.
+                <em>You are just as dependent up on Christ, in order to
+                live a holy life,</em> 
+                as is the branch upon the parent stock for
+                growth and fruitfulness. Apart from Him you have no life. 
+                You have no power to resist temptation or to grow in grace and
+                holiness. Abiding In Him, you may ﬂourish. 
+                Drawing your life from Him, you will not wither nor be fruitless. 
+                You will be like a tree planted by the rivers of water.
+                friends a Christian who has lost hold of Christ has already 
+                <em>died</em> as Spiritual death. But hope is still there for
+                them. Let us came back to our right senses -like the lost 
+                son and turn and get hold onto Christ or Savior. Then we will
+                flourish, be fruitful and like a tree planted by the river.
+
+                </div>
+
+
+            </article>
+
+        </section>
+        <footer>
+            <div class="w3-center">&copy;Molecule</div>
+
+        </footer>
+    </body>
+
+</html>
